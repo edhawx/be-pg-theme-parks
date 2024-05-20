46f579d6928984b89e07de050121dbd47bc52ba9
Katie Luc <k_luc@hotmail.co.uk>
2024-05-20 14:08:23 +0100

    completes q 1-3

--- db/connection.js
+++ db/connection.js
@@ -2,3 +2,14 @@
  * Create your connection to the DB in this file
  * and remember to export it
  */
+
+const {Pool} = require("pg")
+
+
+if(!process.env.PGDATABASE){
+    throw new Error("PGDATABASE is not set up")
+}
+
+const connection = new Pool()
+
+module.exports = connection
--- db/seed.js
+++ db/seed.js
@@ -1,6 +1,6 @@
 const db = require("./connection");
 
-function seed({ parks, rides, stalls }) {
+function seed({ parks, rides, stalls}) {
   return db
     .query("DROP TABLE IF EXISTS rides;")
     .then(() => {
@@ -17,12 +17,32 @@ function seed({ parks, rides, stalls }) {
     })
     .then(() => {
       return createParks();
+    })
+    .then(() => {
+      return createRides();
     });
 }
 
 function createParks() {
   /* Create your parks table in the query below */
-  return db.query("");
-}
+  return db.query(`CREATE TABLE parks (
+    park_id SERIAL PRIMARY KEY,
+    park_name VARCHAR(300) NOT null,
+    year_opened INT NOT null,
+    annual_attendance INT NOT NULL);`)
+    .then((result) => console.log(result));
+};
+
+function createRides() {
+  /* Create your parks table in the query below */
+  return db.query(`CREATE TABLE rides (
+    ride_id SERIAL PRIMARY KEY,
+    park_id INT REFERENCES parks(park_id),
+    year_opened INT NOT NULL,
+    ride_name VARCHAR(100),
+    votes INT NOT NULL);`)
+    .then((result) => console.log(result));
+};
+
 
 module.exports = seed;
